23977b7c774d370175f357e33af6f396ff932706
jc450453 <prabhjotkaur7@my.jcu.edu.au>
2016-12-16 19:41:44 +1000

    ok

--- prac04/tempratures.py
+++ prac04/tempratures.py
@@ -2,3 +2,12 @@ __author__ = 'jc450453'
 temps=[25.2,16.8,35.5,24.9,40,12,17.5]
 temps.sort()
 print(temps)
+
+cool_temp=[12,13,14,15]
+warm_temp=[21,34,56,78]
+temp_in_celcius=cool_temp + warm_temp
+print(temp_in_celcius)
+cool_temp.append(warm_temp)
+print(cool_temp)
+
+
